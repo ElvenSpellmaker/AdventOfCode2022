1255c49340b49f278a5e14c96dacf4f1baf9bfd2
Jack Blower <Jack@elvenspellmaker.co.uk>
2022-12-01 10:35:28 +0000

    feat: day 1 and repo set-up

---
+++ d1p2.php
@@ -0,0 +1,12 @@
+<?php
+
+$input = explode("\n\n", rtrim(file_get_contents('d1.txt')));
+
+$maxSeen = new SplMaxHeap;
+foreach ($input as $key => $val)
+{
+	$sum = array_sum(explode("\n", $val));
+	$maxSeen->insert($sum);
+}
+
+echo $maxSeen->extract() + $maxSeen->extract() + $maxSeen->extract(), "\n";
