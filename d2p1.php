c20e043574735f9a2cdb6e9940025155682f1692
Jack Blower <Jack@elvenspellmaker.co.uk>
2022-12-02 12:13:40 +0000

    fix: Refactor d2 to be even simpler

--- d2p1.php
+++ d2p1.php
@@ -3,26 +3,24 @@
 $input = explode("\n", rtrim(file_get_contents('d2.txt')));
 
 $gameScore = [
-	'AX' => 1 + 3,
-	'AY' => 2 + 6,
-	'AZ' => 3 + 0,
+	'A X' => 1 + 3,
+	'A Y' => 2 + 6,
+	'A Z' => 3 + 0,
 
-	'BX' => 1 + 0,
-	'BY' => 2 + 3,
-	'BZ' => 3 + 6,
+	'B X' => 1 + 0,
+	'B Y' => 2 + 3,
+	'B Z' => 3 + 6,
 
-	'CX' => 1 + 6,
-	'CY' => 2 + 0,
-	'CZ' => 3 + 3,
+	'C X' => 1 + 6,
+	'C Y' => 2 + 0,
+	'C Z' => 3 + 3,
 ];
 
 $score = 0;
 
 foreach ($input as $game)
 {
-	[$chose, $choose] = explode(' ', $game);
-
-	$score += $gameScore[$chose . $choose];
+	$score += $gameScore[$game];
 }
 
 
